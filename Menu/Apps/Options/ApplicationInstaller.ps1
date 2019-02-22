3c09fe127d19bd34855a082e6c54752d1bc352e1
Daniel Imaino <cryptodimaino@gmail.com>
2019-02-21 16:32:27 -0800

    Added Google Chrome to Installer and Uninstaller

--- Menu/Apps/Options/ApplicationInstaller.ps1
+++ Menu/Apps/Options/ApplicationInstaller.ps1
@@ -22,8 +22,8 @@ $url5 = "https://mirror.sfo12.us.leaseweb.net/videolan/vlc/3.0.6/win32/vlc-3.0.6
 
 
 $InstallList = @(
-	"InstallBraveBrowser"
-	#"InstallGoogleChrome",
+	#"InstallBraveBrowser",
+	"InstallGoogleChrome"
 	#"InstallFireFox"
 )
 
@@ -44,7 +44,7 @@ Function CheckIfInstalled($Software)
 	If(-Not $installed32 -and -Not $installed64)
 	{
 		$Value = $TRUE
-		Write-Host "'$Software' NOT is installed.";
+		Write-Host "'$Software' is NOT installed.";
 	} 
 	else
 	{
@@ -62,7 +62,9 @@ Function PinBraveBrowserToTaskBar
 	}
 	else
 	{
-		$Location = Get-Location
+		$Location = Split-Path -Path $PSScriptRoot
+		$Location = Split-Path -Path $Location -Parent
+		$Location = Split-Path -Path $Location -Parent
 		$CMD =  Join-Path -Path $Location -ChildPath "Menu\Tools\Options\syspin.exe"
 		$arg1 = 'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe'
 		$arg2 = 'c:"Pin to Taskbar"'
@@ -75,36 +77,36 @@ Function InstallBraveBrowser()
 {
 	if(CheckIfInstalled('Brave'))
 	{
-	$output1 = "C:\InstallerApps\Bravex64.exe"
-	$url1 = "https://laptop-updates.brave.com/latest/winx64"
-	Write-Host "Downloading Brave Browser x64 Version..."
-	(New-Object System.Net.WebClient).DownloadFile($url1, $output1)
-	
-	Write-Host "Installing Brave Browser x64 Version..."
-	Start-Process -FilePath 'C:\InstallerApps\Bravex64.exe' -PassThru
+		$output1 = "C:\InstallerApps\Bravex64.exe"
+		$url1 = "https://laptop-updates.brave.com/latest/winx64"
+		Write-Host "Downloading Brave Browser x64 Version..."
+		(New-Object System.Net.WebClient).DownloadFile($url1, $output1)
+		
+		Write-Host "Installing Brave Browser x64 Version..."
+		Start-Process -FilePath 'C:\InstallerApps\Bravex64.exe' -PassThru
 
-	$BraveLoop = $false
+		$BraveLoop = $false
 
-	Do
-	{
-		$Bravestatus =  Get-Process -Name "brave" -ErrorAction SilentlyContinue
-		if(!($Bravestatus))
+		Do
 		{
-			Write-Host 'Installing Brave Browser...' ;
-			Start-Sleep -S 2
+			$Bravestatus =  Get-Process -Name "brave" -ErrorAction SilentlyContinue
+			if(!($Bravestatus))
+			{
+				Write-Host 'Installing Brave Browser...' ;
+				Start-Sleep -S 2
+			}
+			else
+			{
+				$BraveLoop = $true
+			}
 		}
-		else
-		{
-			$BraveLoop = $true
-		}
-	}
-	Until($BraveLoop)
-	Start-Sleep -S 1
-	Write-Host "Stopping Brave Browser and Installer..."
-	Stop-Process -Name "BraveUpdate" -Force
-	Stop-Process -Name "brave" -Force
-	PinBraveBrowserToTaskBar
-	Write-Host 'Brave Browser has been installed!' ;
+		Until($BraveLoop)
+		Start-Sleep -S 2
+		Write-Host "Stopping Brave Browser and Installer..."
+		Stop-Process -Name "BraveUpdate" -Force
+		Stop-Process -Name "brave" -Force
+		PinBraveBrowserToTaskBar
+		Write-Host 'Brave Browser has been installed!' ;
 	}
 	else
 	{
@@ -113,6 +115,71 @@ Function InstallBraveBrowser()
 }
 
 
+Function PinChromeBrowserToTaskBar
+{
+	if([System.IO.File]::Exists('%appdata%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar'))
+	{
+		Write-Host "Chrome Browser is already on the taskbar."
+	}
+	else
+	{
+		$Location = Split-Path -Path $PSScriptRoot
+		$Location = Split-Path -Path $Location -Parent
+		$Location = Split-Path -Path $Location -Parent
+		$CMD =  Join-Path -Path $Location -ChildPath "Menu\Tools\Options\syspin.exe"
+		$arg1 = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+		$arg2 = 'c:"Pin to Taskbar"'
+		& $CMD $arg1 $arg2
+		Write-Host "Chrome Browser was pinned to the taskbar."
+	}
+}
+
+Function InstallGoogleChrome()
+{
+	if(CheckIfInstalled('Chrome'))
+	{
+		$output2 = "C:\InstallerApps\Chromex64.exe"
+		$url2 = "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B0DC43149-738D-EB7C-0875-4A907A9D5AD3%7D%26lang%3Den%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/chrome/install/ChromeStandaloneSetup64.exe"
+		#$url2 = "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B5037592E-477A-46DB-207A-2C2C27C53C51%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dtrue%26ap%3Dx64-stable-statsdef_0%26brand%3DGCEB/dl/chrome/install/GoogleChromeEnterpriseBundle64.zip"
+		Write-Host "Downloading Chrome Browser x64 Version..."
+		(New-Object System.Net.WebClient).DownloadFile($url2, $output2)
+		
+		#Expand-Archive -LiteralPath 'C:\InstallerApps\Chromex64.zip' -DestinationPath 'C:\InstallerApps\Chromex64' -Force
+		
+		
+		Write-Host "Installing Chrome Browser x64 Version..."
+		Start-Process -Wait -FilePath 'C:\InstallerApps\Chromex64.exe' -PassThru
+		#msiexec.exe /i "C:\InstallerApps\Chromex64\Installers\GoogleChromeStandaloneEnterprise64.msi" /quiet
+		#$params = '/i', 'C:\InstallerApps\Chromex64\Installers\GoogleChromeStandaloneEnterprise64.msi', '/quiet'
+		#$p = Start-Process 'msiexec.exe' -ArgumentList $params -NoNewWindow -Wait -PassThru
+		#$p.ExitCode
+		#Start-Process -FilePath C:\Windows\System32\msiexec.exe -ArgumentList "/i C:\InstallerApps\Chromex64\Installers\GoogleChromeStandaloneEnterprise64.msi" -wait
+		#msiexec: /i googlechrome.msi /quiet Remove-Item C:\Users\Public\Desktop\Google Chrome.lnk
+		Start-Sleep -S 2
+		
+		Stop-Process -Name "chrome" -Force
+		Write-Host "Stopping Chrome Browser and Installer..."
+		PinChromeBrowserToTaskBar
+		
+		#Remove-Item -path 'C:\Users\Public\Desktop\Google Chrome.lnk'  -Force
+		Write-Host 'Chrome Browser Desktop Icon has been Deleted.' ;
+		Write-Host 'Chrome Browser has been installed!' ;
+		
+	}
+	else
+	{
+		Write-Host 'Chrome Browsers appears to already be installed.'
+	}
+}
 
 # Call the Installs
 $InstallList | ForEach { Invoke-Expression $_ }
+
+
+
+
+
+
+
+
+
--- Menu/Apps/Options/ApplicationUninstaller.ps1
+++ Menu/Apps/Options/ApplicationUninstaller.ps1
@@ -1,6 +1,6 @@
 $UninstallList = @(
-	"UninstallBraveBrowser"
-	#"UninstallGoogleChrome",
+	#"UninstallBraveBrowser",
+	"UninstallGoogleChrome"
 	#"UninstallFireFox"
 )
 
@@ -26,7 +26,7 @@ Function CheckIfInstalled($Software)
     }
 	If(-Not $installed32 -and -Not $installed64)
 	{
-		Write-Host "'$Software' NOT is installed.";
+		Write-Host "'$Software' is NOT installed.";
 		return $TRUE
 	} 
 	else
@@ -41,7 +41,9 @@ Function UnpinBraveBrowserToTaskBar
 {
 	if([System.IO.File]::Exists('%appdata%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar'))
 	{
-		$Location = Get-Location
+		$Location = Split-Path -Path $PSScriptRoot
+		$Location = Split-Path -Path $Location -Parent
+		$Location = Split-Path -Path $Location -Parent
 		$CMD = Join-Path -Path $Location -ChildPath "Menu\Tools\Options\syspin.exe"
 		$arg1 = 'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe'
 		$arg2 = 'c:"Unpin from Taskbar"'
@@ -66,6 +68,40 @@ Function UninstallBraveBrowser()
 		Write-Host "Currently uninstalling Brave Browser..."
 		(Get-ItemProperty -path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\BraveSoftware Brave-Browser').version | ForEach-Object {& ${env:ProgramFiles(x86)}\BraveSoftware\Brave-Browser\Application\$_\Installer\setup.exe --uninstall --multi-install --chrome --system-level --force-uninstall}
 		Write-Host "Brave Browser unintalled..."
+	}
+}
+
+Function UnpinChromeBrowserToTaskBar
+{
+	if([System.IO.File]::Exists('%appdata%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar'))
+	{
+		$Location = Split-Path -Path $PSScriptRoot
+		$Location = Split-Path -Path $Location -Parent
+		$Location = Split-Path -Path $Location -Parent
+		$CMD = Join-Path -Path $Location -ChildPath "Menu\Tools\Options\syspin.exe"
+		$arg1 = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+		$arg2 = 'c:"Unpin from Taskbar"'
+		& $CMD $arg1 $arg2
+	}
+	else
+	{
+		Write-Host "Chrome Browser is not pinned to the taskbar."
+	}
+}
+
+
+Function UninstallGoogleChrome()
+{
+	if(CheckIfInstalled('Google Chrome'))
+	{
+		Write-Host "It appears that Chrome Browsers is not installed on this system..."
+	}
+	else
+	{
+		UnpinChromeBrowserToTaskBar
+		Write-Host "Currently uninstalling Chrome Browser..."
+		(Get-ItemProperty -path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{FB57C140-533A-3441-B7D2-9EC87EC1B6D2}').version | ForEach-Object {& ${env:ProgramFiles(x86)}\Google\Chrome\Application\72.0.3626.119\Installer\setup.exe --uninstall --multi-install --chrome --system-level --force-uninstall}
+		Write-Host "Chrome Browser unintalled..."
 	}
 }
 
